d81896cd162d42669e4aa1b2f87505b2371188d9
asafLaptop <gorff5@gmail.com>
2017-02-08 00:47:54 +0200

    update data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -76,227 +76,254 @@
     // Your custom JavaScript goes here
 
     $(document).ready(function () {
-            var canvas = document.getElementById('mainCanvas');
-            var context = canvas.getContext("2d");
-            var clickX = new Array();
-            var clickY = new Array();
-            var clickX2 = new Array();
-            var clickY2 = new Array();
-            var clickDrag = new Array();
-            var paint;
-            var middleXY = 250;
-        var data = [];
-            printSquare();
-        initData();
+        var canvas = document.getElementById('mainCanvas');
+        var context = canvas.getContext("2d");
+        var clickX = new Array();
+        var clickY = new Array();
+        var clickX2 = new Array();
+        var clickY2 = new Array();
+        var clickDrag = new Array();
+        var paint;
+        var middleXY = 250;
+        var data = initData();
+        printSquare();
 
-            $('#mainCanvas').mousedown(function (e) {
-                var mouseX = e.pageX - this.offsetLeft;
-                var mouseY = e.pageY - this.offsetTop;
 
-                paint = true;
-                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        $('#mainCanvas').mousedown(function (e) {
+            var mouseX = e.pageX - this.offsetLeft;
+            var mouseY = e.pageY - this.offsetTop;
+
+            paint = true;
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            redraw();
+        });
+
+        $('#mainCanvas').mousemove(function (e) {
+            if (paint) {
+                console.log(e.pageY);
+                addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
                 redraw();
-            });
-
-            $('#mainCanvas').mousemove(function (e) {
-                if (paint) {
-                    console.log(e.pageY);
-                    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-                    redraw();
-                }
-            });
-
-            $('#mainCanvas').mouseup(function (e) {
-                paint = false;
-            });
-
-
-            $('#mainCanvas').mouseleave(function (e) {
-                paint = false;
-            });
-
-
-            function addClick(x, y, dragging) {
-
-
-                clickX.push(x);
-                clickY.push(y);
-                clickDrag.push(dragging);
-
-                // x = x - middleXY;
-                y = middleXY - y;
-
-                clickX2.push(x);
-                clickY2.push(y);
-
             }
+        });
 
-            function redraw() {
-                // context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
-
-                context.strokeStyle = "#df4b26";
-                context.lineJoin = "round";
-                context.lineWidth = 5;
-                let oldX;
-                let oldY;
-
-                for (var i = 0; i < clickX.length; i++) {
-                    context.beginPath();
-                    if (clickDrag[i] && i) {
-                        context.moveTo(clickX[i - 1], clickY[i - 1]);
-                    } else {
-                        context.moveTo(clickX[i] - 1, clickY[i]);
-                    }
-                    context.lineTo(clickX[i], clickY[i]);
-                    context.closePath();
-                    context.stroke();
-                }
-            }
+        $('#mainCanvas').mouseup(function (e) {
+            paint = false;
+        });
 
 
-            $('#analyze').click(()=> {
-                let avgX = calcAvg(clickX);
-                let avgY = calcAvg(clickY2);
-                let deviationNumber = clickY2.length / 2;
-                let maxY = Math.max(...clickY2);
-                let minY = Math.min(...clickY2);
-                // let maxX = Math.max(...clickX2);
-                // let minX = Math.min(...clickX2);
-
-                let isGoingUp = checkGoingUp(avgY, deviationNumber, maxY);
-                let isGoingDown = checkGoingDown(avgY, deviationNumber, minY);
-                let isUnderLine = checkUnderLine(clickY2, clickX2, avgY, minY);
-                let isMiddleLine = checkMiddleLine(clickY2, clickX2, avgY, minY, maxY);
-
-                console.log('isGoingUp: ' + isGoingUp.toString());
-                console.log('isGoingDown: ' + isGoingDown.toString());
-                console.log('isUnderLine: ' + isUnderLine.toString());
-                console.log('isMiddleLine: ' + isMiddleLine.toString());
-
-                let detectArray = [isGoingUp,isGoingDown,isUnderLine,isMiddleLine];
-
-                showMessage(detectArray);
+        $('#mainCanvas').mouseleave(function (e) {
+            paint = false;
+        });
 
 
-            });
-
-            function checkGoingUp(avgY, deviationNumber, max) {
-
-                return max - avgY > deviationNumber;
-            }
-
-            function checkGoingDown(avgY, deviationNumber, min) {
-
-                return avgY - min > deviationNumber;
-            }
-
-            function calcAvg(array) {
-                let sum = 0;
-
-                for (let i = 0; i < array.length; i++) {
-                    sum += array[i];
-                }
-
-                return sum / array.length;
-            }
-
-            //This func search straight line,
-            //if found we check if this line is a underline
-            function checkUnderLine(clickY2, clickX2, avgY, minY) {
-                let deviationNumber = 15;
-                let lineMinHeight = checkStraightLine(clickY2, clickX2);
-                if (lineMinHeight) {
-                    if (lineMinHeight < calcAvg([avgY - deviationNumber, minY])) { //I make avg to make some deviation
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            function checkMiddleLine(clickY2, clickX2, avgY, minY, maxY) {
-                let deviationNumber = 15;
-                let lineMinHeight = checkStraightLine(clickY2, clickX2);
-                if (lineMinHeight) {
-                    if (lineMinHeight > calcAvg([avgY - deviationNumber, minY]) && lineMinHeight < calcAvg([avgY - deviationNumber, maxY])) { //I make avg to make some deviation
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            // If found Straight line minimum line height null
-            function checkStraightLine(clickY2, clickX2) {
-                let deviationNumber = 2;
-                let prevY = 0; // some temp value, used for calc in the for loop
-                let prevX = 0; // some temp value, used for calc in the for loop
-                let counter = 0;
-                let lineLength = 100; // the line we will find need to be at least x lengyh
-                let longestLineFoundNumber = 0; // the height value that the counter reached
-                let lastIndex = 0; //The last index of the longestLineFoundNumber found
+        function addClick(x, y, dragging) {
 
 
-                for (let i = 0; i < clickY2.length; i++) {
-                    if ((clickY2[i] >= prevY - deviationNumber) &&
-                        (clickY2[i] <= prevY + deviationNumber) &&
-                        (clickX2[i] != prevX)) {
-                        counter++;
-                        if (counter > longestLineFoundNumber) {
-                            longestLineFoundNumber = counter;
-                            lastIndex = i;
-                        }
-                    }
-                    else {
-                        counter = 0;
-                    }
-                    prevY = clickY2[i];
-                    prevX = clickX2[i];
-                }
+            clickX.push(x);
+            clickY.push(y);
+            clickDrag.push(dragging);
 
-                //if line found return avg of the max and min indexes
-                return clickX2[lastIndex] - clickX2[lastIndex - longestLineFoundNumber] > lineLength ? calcAvg([clickY2[lastIndex], clickY2[lastIndex - longestLineFoundNumber]]) : null;
-            }
+            // x = x - middleXY;
+            y = middleXY - y;
 
-            function printSquare() {
-                context.beginPath();
-                context.rect(300, 200, 400, 100);
-                context.stroke();
-            }
+            clickX2.push(x);
+            clickY2.push(y);
 
-            // resize the canvas to fill browser window dynamically
-            window.addEventListener('resize', resizeCanvas, false);
-
-            function resizeCanvas() {
-                canvas.width = window.innerWidth;
-                canvas.height = 500;
-
-                /**
-                 * Your drawings need to be inside this function otherwise they will be reset when
-                 * you resize the browser window and the canvas goes will be cleared.
-                 */
-                drawStuff();
-            }
-
-            resizeCanvas();
-
-            function drawStuff() {
-                // do your drawing stuff here
-            }
-
-        function initData() {
-            $.getJSON("data.json", function (json) {
-                data = json;
-            });
         }
 
-        function showMessage(detectArray) {
+        function redraw() {
+            // context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
 
+            context.strokeStyle = "#df4b26";
+            context.lineJoin = "round";
+            context.lineWidth = 5;
+            let oldX;
+            let oldY;
+
+            for (var i = 0; i < clickX.length; i++) {
+                context.beginPath();
+                if (clickDrag[i] && i) {
+                    context.moveTo(clickX[i - 1], clickY[i - 1]);
+                } else {
+                    context.moveTo(clickX[i] - 1, clickY[i]);
+                }
+                context.lineTo(clickX[i], clickY[i]);
+                context.closePath();
+                context.stroke();
+            }
+        }
+
+
+        $('#analyze').click(() => {
+            let avgX = calcAvg(clickX);
+            let avgY = calcAvg(clickY2);
+            let deviationNumber = clickY2.length / 2;
+            let maxY = Math.max(...clickY2);
+            let minY = Math.min(...clickY2);
+            // let maxX = Math.max(...clickX2);
+            // let minX = Math.min(...clickX2);
+
+            let isGoingUp = checkGoingUp(avgY, deviationNumber, maxY);
+            let isGoingDown = checkGoingDown(avgY, deviationNumber, minY);
+            let isUnderLine = checkUnderLine(clickY2, clickX2, avgY, minY);
+            let isMiddleLine = checkMiddleLine(clickY2, clickX2, avgY, minY, maxY);
+
+            console.log('isGoingUp: ' + isGoingUp.toString());
+            console.log('isGoingDown: ' + isGoingDown.toString());
+            console.log('isUnderLine: ' + isUnderLine.toString());
+            console.log('isMiddleLine: ' + isMiddleLine.toString());
+
+            let detectArray = [
+                {
+                    key: 'isGoingUp',
+                    value: isGoingUp
+                },
+                {
+                    key: 'isGoingDown',
+                    value: isGoingDown
+                },
+                {
+                    key: 'isUnderLine',
+                    value: isUnderLine
+                },
+                {
+                    key: 'isMiddleLine',
+                    value: isMiddleLine
+                }
+            ];
+
+            showMessage(detectArray);
+
+        });
+
+        function checkGoingUp(avgY, deviationNumber, max) {
+
+            return max - avgY > deviationNumber;
+        }
+
+        function checkGoingDown(avgY, deviationNumber, min) {
+
+            return avgY - min > deviationNumber;
+        }
+
+        function calcAvg(array) {
+            let sum = 0;
+
+            for (let i = 0; i < array.length; i++) {
+                sum += array[i];
+            }
+
+            return sum / array.length;
+        }
+
+        //This func search straight line,
+        //if found we check if this line is a underline
+        function checkUnderLine(clickY2, clickX2, avgY, minY) {
+            let deviationNumber = 15;
+            let lineMinHeight = checkStraightLine(clickY2, clickX2);
+            if (lineMinHeight) {
+                if (lineMinHeight < calcAvg([avgY - deviationNumber, minY])) { //I make avg to make some deviation
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function checkMiddleLine(clickY2, clickX2, avgY, minY, maxY) {
+            let deviationNumber = 15;
+            let lineMinHeight = checkStraightLine(clickY2, clickX2);
+            if (lineMinHeight) {
+                if (lineMinHeight > calcAvg([avgY - deviationNumber, minY]) && lineMinHeight < calcAvg([avgY - deviationNumber, maxY])) { //I make avg to make some deviation
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // If found Straight line minimum line height null
+        function checkStraightLine(clickY2, clickX2) {
+            let deviationNumber = 2;
+            let prevY = 0; // some temp value, used for calc in the for loop
+            let prevX = 0; // some temp value, used for calc in the for loop
+            let counter = 0;
+            let lineLength = 100; // the line we will find need to be at least x lengyh
+            let longestLineFoundNumber = 0; // the height value that the counter reached
+            let lastIndex = 0; //The last index of the longestLineFoundNumber found
+
+
+            for (let i = 0; i < clickY2.length; i++) {
+                if ((clickY2[i] >= prevY - deviationNumber) &&
+                    (clickY2[i] <= prevY + deviationNumber) &&
+                    (clickX2[i] != prevX)) {
+                    counter++;
+                    if (counter > longestLineFoundNumber) {
+                        longestLineFoundNumber = counter;
+                        lastIndex = i;
+                    }
+                }
+                else {
+                    counter = 0;
+                }
+                prevY = clickY2[i];
+                prevX = clickX2[i];
+            }
+
+            //if line found return avg of the max and min indexes
+            return clickX2[lastIndex] - clickX2[lastIndex - longestLineFoundNumber] > lineLength ? calcAvg([clickY2[lastIndex], clickY2[lastIndex - longestLineFoundNumber]]) : null;
+        }
+
+        function printSquare() {
+            context.beginPath();
+            context.rect(300, 200, 400, 100);
+            context.stroke();
+        }
+
+        // resize the canvas to fill browser window dynamically
+        window.addEventListener('resize', resizeCanvas, false);
+
+        function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = 500;
+
+            /**
+             * Your drawings need to be inside this function otherwise they will be reset when
+             * you resize the browser window and the canvas goes will be cleared.
+             */
+            drawStuff();
+        }
+
+        resizeCanvas();
+
+        function drawStuff() {
+            // do your drawing stuff here
+        }
+
+
+        function showMessage(detectArray) {
+            for (let detect of detectArray) {
+                if (detect.value) {
+                    alert(data[detect.key]);
+                    return;
+                }
+            }
         }
     });
 
 
-
-
-
-
-
 })();
+
+
+function initData() {
+    return {
+        "fullName": "אתם אנשים פתוחים, גלויים, בעלי שמחת חיים, אין לכם מה להסתיר ומה שרואים - זה מה שיש. אגב, המרחק בין השמות בהחלט משנה. ככל שהשם הפרטי קרוב יותר לשם המשפחה, כך התקשורת שלכם עם משפחתכם טובה יותר. ככול שהשם הפרטי רחוק יותר משם המשפחה (מרחק של אות וחצי עד שתיים בערך) – תתחילו לעבוד על הקשרים המשפחתיים שלכם.",
+        "firstName": "אתם אגואיסטים לא קטנים. אתם נוהגים להשקיע בעצמכם יותר מאשר במשפחתכם ואתם רוצים להראות לכל העולם עד כמה אתם יודעים לעשות הכל לבד. יכול להיות שסיימתם לאחרונה תיכון, לחלופין יכול להיות שלא מזמן התגרשתם ואתם רוצים להראות עד כמה אתם עצמאיים.",
+        "notClear": "כנראה שיש לכם מה להסתיר. להוציא מכם משהו על עצמכם – קשה מאוד.",
+        "circled": "על חומת סין שמעתם? אתם מקיפים את עצכם בחומות ולא נותנים לאף אחד להתקרב אליכם, אתם חשדנים ולא בוטחים באחרים. מאיפה נובע הריחוק הזה?",
+        "isGoingUp": "אם חתימתכם עולה למעלה, אתם אנשים שעובדים עם השכל ועם ההיגיון. אתם מנכ\"לים או משהו?",
+        "isGoingDown": "אם חתימתכם יורדת למטה, אתם אנשים שעובדים לפי הרגש. אתם יצריים, יצירתיים ואוהבים כסף.",
+        "isMiddleLine": "אתם לא כל כך מסתדרים עם עצמכם ולא מאמינים ביכולות שלכם. לכן אתם נוטים לבטל את עצמכם. שימו פס על משהו אחר, לא על עצמכם.",
+        "isUnderLine": "אתם מחפשים יציבות בחייכם. לא תיכנסו לתחומים שאתם לא שוחים בהם. כמו כן, אהבת הבמה לא פוסחת עליכם. אתם שוקלים קריירה בימתית?",
+        "dotInEnd": "אתם אנשים של כאן ועכשיו, ורק תוצאות מיידות מביאות אתכם לידי סיפוק. אתם מחושבים ודייקנים ויודעים לעמוד על שלכם.",
+        "acronyms": "אם חתמתם בראשי תיבות, אתם נוהגים לקצר תהליכים - ועושים את זה בצורה מדהימה.",
+    };
+}
